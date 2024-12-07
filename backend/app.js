f794192006d11dc54fcb5a2006deff80ca9a3407
PrasanthPandi008 <prasanthpandi@bloomitsolutions.com>
2024-12-07 15:57:25 +0530

    starting code

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+
+const express= require('express');
+const mongoose=require('mongoose');
+
+const app=express();
+
+const product
+
+app.listen(8000,()=>{
+    console.log(`Server started at localhost:5000`);
+})
+
+
+
